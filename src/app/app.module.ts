a02bbdf017f4ee1e9548bcd5fb2a3b0d336142a2
nitheeshmashraf <nitheeshmashraf@gmail.com>
2017-09-04 02:35:45 +0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import  { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashComponent } from './dash/dash.component';
+
+import {  UserService } from './user.service';
+
+import { AuthGuard } from './auth.guard';
+
+const approutes:Routes=[
+  {
+    path: 'dash',
+    canActivate:[ AuthGuard ],
+    component:DashComponent
+  },
+  {
+    path: '',
+    component:LoginFormComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -16,9 +33,13 @@ import { DashComponent } from './dash/dash.component';
     DashComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(approutes)
   ],
-  providers: [],
+  providers: [
+    UserService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
